f545c2e63559fbedeb8ddf4ca1b497c961a03e7e
hendisantika <hendisantika@yahoo.co.id>
2024-05-16 17:05:43 +0700

    Add Contact Model🫸🌀✏️📗🐧🐳

---
+++ es-sample/src/main/java/id/my/hendisantika/model/Contact.java
@@ -0,0 +1,63 @@
+package id.my.hendisantika.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : elasticsearch-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/16/24
+ * Time: 17:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class Contact {
+    String name;
+    String email;
+    Integer age;
+
+    public Contact() {
+    }
+
+    public Contact(String name, String email, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return Objects.equals(name, contact.name) && Objects.equals(email, contact.email) && Objects.equals(age, contact.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, email, age);
+    }
+}
