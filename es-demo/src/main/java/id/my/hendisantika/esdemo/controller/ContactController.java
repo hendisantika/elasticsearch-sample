ea958e1d7a3a4de4f9066bc71d92a5ed91acd73e
hendisantika <hendisantika@yahoo.co.id>
2024-05-16 17:24:59 +0700

    Add list in ContactController🫸🌀✏️📗🐧🐳

--- es-demo/src/main/java/id/my/hendisantika/esdemo/controller/ContactController.java
+++ es-demo/src/main/java/id/my/hendisantika/esdemo/controller/ContactController.java
@@ -1,8 +1,14 @@
 package id.my.hendisantika.esdemo.controller;
 
+import id.my.hendisantika.esdemo.dto.ContactResponse;
+import id.my.hendisantika.esdemo.model.Contact;
 import id.my.hendisantika.esdemo.service.ContactService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +25,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class ContactController {
 
     private final ContactService contactService;
+
+    @GetMapping("/contacts/list")
+    public ResponseEntity<ContactResponse> list() {
+        ContactResponse response = new ContactResponse();
+        try {
+            List<Contact> result = contactService.list();
+            response.setList(result);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            response.setError(e.getMessage());
+            return ResponseEntity.internalServerError().body(response);
+        }
+    }
 }
